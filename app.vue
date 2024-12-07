cc74f0fa7aff0d4396671b626d12007ae792be24
Aleksey Grishkin <kvgyzqk1996@yandex.ru>
2024-12-07 22:21:06 +0300

    Сделаны общие настройки проекта

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hello World</h1>
   </div>
 </template>
+
+<style lang="scss">
+h1 {
+  @include Title;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { provider } from "std-env";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "@vueuse/nuxt",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@pinia/nuxt",
+    "dayjs-nuxt",
+    "nuxt-typed-router",
+    "nuxt-file-storage",
+  ],
+  families: [{ name: "montserrat", provider: "google" }],
+  css: ["~/assets/scss/global.scss"],
+  vite: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @use "~/assets/scss/_vars.scss" as *;
+        @use "~/assets/scss/_mixins.scss" as *;`,
+      },
+    },
+  },
+});
