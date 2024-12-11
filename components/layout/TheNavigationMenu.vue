865e612cab31f646a6a041d84cd2475042168592
Aleksey Grishkin <kvgyzqk1996@yandex.ru>
2024-12-11 21:34:30 +0300

    Добавил изменения в конфиги и картинки.

---
+++ components/layout/TheNavigationMenu.vue
@@ -0,0 +1,28 @@
+<template></template>
+
+<script setup>
+defineOptions({
+  name: 'TheHeader',
+});
+
+const NAVIGATION_CONFIG = [
+  {
+    link: '/catalog',
+    img: '/catalog.svg',
+  },
+  {
+    link: '/entries',
+    img: '/entries.svg',
+  },
+  {
+    link: '/certificates',
+    img: '/certificates.svg',
+  },
+  {
+    link: '/reviews',
+    img: '/reviews.svg',
+  },
+];
+</script>
+
+<style module lang="scss"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    "@vueuse/nuxt",
-    "@nuxt/fonts",
-    "@nuxt/image",
-    "@pinia/nuxt",
-    "dayjs-nuxt",
-    "nuxt-typed-router",
-    "nuxt-file-storage",
-    "@nuxt/eslint",
+    '@vueuse/nuxt',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    'dayjs-nuxt',
+    'nuxt-typed-router',
+    'nuxt-file-storage',
+    '@nuxt/eslint',
   ],
-  families: [{ name: "montserrat", provider: "google" }],
-  css: ["~/assets/scss/global.scss"],
+  families: [{ name: 'montserrat', provider: 'google' }],
+  css: ['~/assets/scss/global.scss'],
   vite: {
     css: {
       preprocessorOptions: {
